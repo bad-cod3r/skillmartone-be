b502448a4f3ccbc40d7bc8584e5e78350c4deeed
Isrmzz04 <isramiraza004@gmail.com>
2025-01-17 23:42:56 +0700

    fix cors

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+require_once __DIR__ . '/app/config/cors.php';
+setupCORS();
 header("Content-Type: application/json");
 
 require_once __DIR__ . '/app/config/database.php';
